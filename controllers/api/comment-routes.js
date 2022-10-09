0b41dc1f49872a4aa412efcb5674bcb73fdcfd54
Daniela Lopez <dlope0831@gmail.com>
2022-10-09 18:08:24 -0500

    added more handlebar pages

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -19,17 +19,21 @@ router.get('/', (req, res) => {
       });
   });
 
-router.post('/', (req, rest)=> {
+
+router.post('/', (req, res)=> {
+  if (req.session){
     Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
+      comment_text: req.body.comment_text,
+      user_id: req.body.user_id,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id
+  })
         .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
-        })
+        });
+      }
 });
 
 router.delete('/:id', (req, res) => {
